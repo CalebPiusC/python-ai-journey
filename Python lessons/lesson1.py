4391dbcfb5bbe3e2befc2d61199f60fb1ea75408
PusCaleb <calebpius101@gmail.com>
2026-09-04 20:13:47 +0100

    Day 2

---
+++ Python lessons/lesson1.py
@@ -0,0 +1,24 @@
+greeting ='hello, world!'
+print(greeting)
+line01 = "**************" # header/fooeter
+line02 = "*            *" # re-use
+line03 = "*  WELCOME   *"
+line04 = "**************"
+# start with a blank line
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line04)
+
+meaning = 8
+print('')
+
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not today')
+
+# Tenary Operator
+print('Right on!') if meaning > 10 else print('Not today')
