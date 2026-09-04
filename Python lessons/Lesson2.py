115ddd448c0746f62ffbeccc84c2e6d26f5e2496
PusCaleb <calebpius101@gmail.com>
2026-09-04 20:17:11 +0100

    Day2

---
+++ Python lessons/Lesson2.py
@@ -0,0 +1,30 @@
+#String data type
+
+#Literal assignment
+first = "Dave"
+last = "Gray" 
+
+# print(type(first))
+# print(type(first)== str) 
+# print(isinstance(first, str))
+
+# # constructor function 
+# pizza = str("pepperoni")
+# print(type(pizza))
+# print(type(pizza)== str) 
+# print(isinstance(pizza, str))
+
+
+# concatenation 
+fullname = first + "" + last
+print(fullname)
+
+fullname += "!"
+print(fullname)
+
+# casting a number to a string
+decade = str(1980)
+print(type(decade))
+print(decade)
+
+statement = "i like rock music from"
