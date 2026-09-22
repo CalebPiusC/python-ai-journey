4c2f15112d6a5369e6dcd0a1ae340c719595474f
PusCaleb <calebpius101@gmail.com>
2026-09-22 07:26:52 +0100

    if statements

---
+++ Python lessons/lesson7.py
@@ -0,0 +1,14 @@
+
+is_hot = False
+is_cold = True
+
+if is_hot :
+    print("it's a hot day")
+    print("drink plenty of water")
+elif is_cold: 
+    print("it's a cold day")
+    print("wear warm clothes")
+else:
+    print("its a lovely day")
+
+print("enjoy your day")
